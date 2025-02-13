dc0a953d669a190e608c05b7834dffbdd4d7f230
Murali Koushik Desu <142626073+MuraliKoushikDesu@users.noreply.github.com>
2025-02-13 05:22:16 -0600

    Update OrdersController.cs

--- OnlineGrocery/Controllers /OrdersController.cs
+++ OnlineGrocery/Controllers /OrdersController.cs
@@ -2,7 +2,6 @@
 using MongoDB.Bson;
 using OnlineGrocery.Models;
 using OnlineGrocery.services;
-
 namespace OnlineGrocery.Controllers
 {
     [Route("api/[controller]")]
@@ -10,7 +9,6 @@ namespace OnlineGrocery.Controllers
     public class OrdersController : Controller
     {
         private readonly MongoConnectionService _mongoConnService;
-
         public OrdersController(MongoConnectionService mongoConnService)
         {
             _mongoConnService = mongoConnService;
@@ -20,7 +18,6 @@ namespace OnlineGrocery.Controllers
         {
             try
             {
-
                 var user = _mongoConnService.GetAllOrders();
                 return user;
             }
@@ -33,8 +30,7 @@ namespace OnlineGrocery.Controllers
         public List<ReturnedOrder> getAllCustomerOrders(string customerId)
         {
             try
-            {
-                
+            {   
                 var user = _mongoConnService.GetAllCustomerOrders(customerId);
                 return user;
             }
@@ -43,14 +39,11 @@ namespace OnlineGrocery.Controllers
                 return null;
             }
         }
-
-
             [HttpGet("getAllDeliveredOrders")]
         public List<GetOrdersModel> getAllDeliveredOrders(string deliveryExecutiveId)
         {
             try
             {
-
                 var user = _mongoConnService.GetAllDeliveredOrders(deliveryExecutiveId);
                 return user;
             }
@@ -64,7 +57,6 @@ namespace OnlineGrocery.Controllers
         {
             try
             {
-
                 var categories = _mongoConnService.GetAllCategories();
                 return categories;
             }
@@ -78,7 +70,6 @@ namespace OnlineGrocery.Controllers
         {
             try
             {
-
                 var categories = _mongoConnService.GetAllProductsByCategories(Category);
                 return categories;
             }
@@ -87,13 +78,11 @@ namespace OnlineGrocery.Controllers
                 return null;
             }
         }
-
         [HttpPost("addToCart")]
         public async Task<String> addtocart(CartModel cartDetails)
         {
             try
             {
-
                 await _mongoConnService.AddToCart(cartDetails.ToBsonDocument());
                 var result = new BsonDocument("success", "Added");
                 return result.ToString();
@@ -103,18 +92,12 @@ namespace OnlineGrocery.Controllers
                 var result = new BsonDocument("error", ex.Message);
                 return result.ToString();
             }
-            
-            
-            
         }
-
-       
         [HttpPost("placeOrder")]
         public async Task<String> placeOrder(PlaceOrder details,string useremail)
         {
             try
             {
-
                 await _mongoConnService.PlaceOrder(details,useremail);
                 var result = new BsonDocument("success", "Added");
                 return result.ToString();
@@ -124,18 +107,12 @@ namespace OnlineGrocery.Controllers
                 var result = new BsonDocument("error", ex.Message);
                 return result.ToString();
             }
-
-
-
         }
-
         [HttpPost("addProduct")]
         public async Task<String> addProduct(ProductsModel productDetails)
         {
             try
             {
-
-
                 await _mongoConnService.AddProduct(productDetails.ToBsonDocument());
                 var result = new BsonDocument("success", "Added");
                 return result.ToString();
@@ -146,16 +123,11 @@ namespace OnlineGrocery.Controllers
                 return result.ToString();
             }
         }
-
-
-
         [HttpPost("addCategory")]
         public async Task<String> addCategory(Categories categoryDetails)
         {
             try
             {
-
-
                 await _mongoConnService.AddCategory(categoryDetails.ToBsonDocument());
                 var result = new BsonDocument("success", "Added");
                 return result.ToString();
@@ -166,14 +138,11 @@ namespace OnlineGrocery.Controllers
                 return result.ToString();
             }
         }
-
-
         [HttpGet("getCart")]
         public List<GetCartModel> getAllCartDetails(String UserEmail)
         {
             try
             {
-
                 var details =  _mongoConnService.GetAllCartDetails(UserEmail);
                 return details;
             }
@@ -182,6 +151,5 @@ namespace OnlineGrocery.Controllers
                 return null;
             }
         }
-
     }
 }
